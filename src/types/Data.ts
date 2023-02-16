45a014105ec7545983bed9cabba6a41764d91a39
sephclt <josephajero22@gmail.com>
2023-02-16 14:26:50 +0800

    data available in dynamic pages

--- src/types/Data.ts
+++ src/types/Data.ts
@@ -1,8 +1,20 @@
-export type PokemonDataType = {
+export type PokemonResultType = {
   name: string;
   url: string;
 };
 
 export type PokemonListType = {
   results: PokemonDataType[];
+};
+
+export type PokemonDataType = {
+  name: string;
+  order: number;
+  sprites: {
+    other: {
+      'official-artwork': {
+        front_default: string;
+      };
+    };
+  };
 };
